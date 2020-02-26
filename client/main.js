6adb07d195611cbd20b84683a14f96188e2b7569
Emil Ahlbäck <ahlback.emil@gmail.com>
2020-02-26 12:22:11 +0100

    basic canvas

--- client/main.js
+++ client/main.js
@@ -1,8 +1,24 @@
 document.addEventListener("DOMContentLoaded", init)
 
+let x = 0, y = 0;
+let ctx;
 function init() {
-    const canvas = document.querySelector("#game-plan")
-    canvas.width = "500px"
-    canvas.height = "500px"
+    const canvas = document.querySelector("#game-canvas")
+    canvas.width = 500
+    canvas.height = 500
+
+    ctx = canvas.getContext("2d")
+
+    setInterval(() => {
+        x++
+        y++
+    }, 10)
     
+    draw();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, 500, 500)
+    ctx.fillRect(x, y, 50, 50);
+    window.requestAnimationFrame(draw)
 }
