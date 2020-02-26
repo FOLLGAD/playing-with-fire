f5349f09f21ed786e26ef69e2fa716175be823e1
Emil Ahlbäck <ahlback.emil@gmail.com>
2020-02-26 12:13:00 +0100

    express setup

---
+++ client/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", init)
+
+function init() {
+    const canvas = document.querySelector("#game-plan")
+    canvas.width = "500px"
+    canvas.height = "500px"
+    
+}
