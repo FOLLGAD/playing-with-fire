c4ec83e2621e240a4af46af9ddfc3870cee5edfd
Emil Ahlbäck <emil@tentium.se>
2020-03-06 16:27:48 +0100

    fixed game leaving

--- client/views/Game.vue
+++ client/views/Game.vue
@@ -17,11 +17,14 @@ export default {
     this.gameCanvas = this.$refs.gamecanvas;
     this.socket = await this.$root.socket;
 
-    this.socket.send("join-game", this.$route.params.gameid)
-
     this.socket.on("joined-game", data => {
       init(this.socket, this.gameCanvas, data.entities);
     });
+    this.socket.on("not-found", () => {
+      this.$router.push(`/gamerooms`);
+    });
+
+    this.socket.send("join-game", this.$route.params.gameid)
   },
   beforeDestroy() {
     if (this.socket) this.socket.send('leave-game')
